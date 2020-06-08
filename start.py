de3b258d550d360b5195cc532b15f5a97746a263
htmboss <63917743+htmboss@users.noreply.github.com>
2020-06-08 22:09:45 +0300

    Add files via upload

---
+++ start.py
@@ -0,0 +1,6 @@
+city=input("όνομα πόλης ")
+a=float (input ("θερμοκρασία 1η "))
+b=float (input ("θερμοκρασία 2η "))
+c=float (input ("θερμοκρασία 3η "))
+avg=(a+b+c)/3.
+print("Η μέση θερμοκρασία είναι:"),avg
